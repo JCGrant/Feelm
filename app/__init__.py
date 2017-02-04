9f257b230f7216eb6ebb6fec86edae728538cbcf
JCGrant <jamescolin.grant@gmail.com>
2017-02-04 17:37:11 +0000

    Add Microsoft Cognitive Emotion API

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,8 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
+app.config['COGNITIVE_URL'] = 'https://westus.api.cognitive.microsoft.com/emotion/v1.0/recognize'
+app.config['COGNITIVE_KEY'] = os.environ['COGNITIVE_KEY']
 
 from app import views
--- app/views.py
+++ app/views.py
@@ -1,6 +1,16 @@
-from flask import render_template
+from flask import render_template, jsonify
 from app import app
+import requests
 
 @app.route('/')
 def home():
     return render_template('home.html')
+
+@app.route('/process/<path:url>', methods=['POST'])
+def process(url):
+    api_url = app.config['COGNITIVE_URL']
+    api_key = app.config['COGNITIVE_KEY']
+    json = { 'url': url }
+    headers = { 'Ocp-Apim-Subscription-Key': api_key }
+    response = requests.post(api_url, json=json, headers=headers)
+    return jsonify(response.json())
