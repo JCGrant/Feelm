6effe89242e7fafc931a2fd9804e9916d5d3877d
JCGrant <jamescolin.grant@gmail.com>
2017-02-05 19:39:29 +0000

    Integrate Lydia's HTML/CSS with app

--- app/views.py
+++ app/views.py
@@ -1,10 +1,27 @@
-from flask import render_template, jsonify
+from flask import render_template, jsonify, abort
 from app import app, cognitive_api
 import requests
 
+EMOTIONS = [
+    'anger',
+    'contempt',
+    'disgust',
+    'fear',
+    'happiness',
+    'neutral',
+    'sadness',
+    'surprise',
+]
+
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', emotions=EMOTIONS)
+
+@app.route('/emotion/<emotion>')
+def emotions(emotion):
+    if emotion not in EMOTIONS:
+        abort(404)
+    return render_template('emotion.html', emotion=emotion)
 
 @app.route('/process/<path:url>', methods=['POST'])
 def process(url):
