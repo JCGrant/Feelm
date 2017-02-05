9d8945e02d107ab9a3997a32999efbdbcb837f5b
JCGrant <jamescolin.grant@gmail.com>
2017-02-05 01:17:09 +0000

    Update models.py

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,8 @@ from cognitive_api import CognitiveApi
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['SQLALCHEMY_DATABASE_URI']
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
 db = SQLAlchemy(app)
 
